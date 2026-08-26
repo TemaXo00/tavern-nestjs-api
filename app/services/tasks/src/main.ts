689ed3bd7e5c8f1249a6f9058d368b8ad50261ee
temaxo <melnikov.artem294@gmail.com>
2026-08-26 14:16:36 +0300

    [feat] Removed tasks service

--- app/services/tasks/src/main.ts
+++
@@ -1,9 +0,0 @@
-import {microserviceLaunch} from "@temaxo00/nx-nest-bootstrap";
-
-import { AppModule } from './app.module';
-
-void microserviceLaunch<AppModule>({
-  name: 'Tasks',
-  module: AppModule,
-  logs: true,
-})
