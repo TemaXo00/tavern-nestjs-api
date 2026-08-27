db09bbc7e61eeb0433099a13f07c2944941d7b4b
temaxo <melnikov.artem294@gmail.com>
2026-08-27 18:57:43 +0300

    [feat] Updated gateway core module for support any gRPC microservices

--- app/libs/gateway-core/src/lib/gateway-core.module.ts
+++ app/libs/gateway-core/src/lib/gateway-core.module.ts
@@ -1,7 +1,40 @@
-import {Module} from '@nestjs/common';
+import { join } from 'path';
+
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  ClientsModule,
+  Transport,
+  GrpcOptions,
+} from '@nestjs/microservices';
+import { workspaceRoot } from 'nx/src/utils/workspace-root';
+
+const SERVICES: string[] = ['AUTH'];
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.registerAsync(
+      SERVICES.map((service) => ({
+        name: `${service}_CLIENT`,
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService): GrpcOptions => ({
+          transport: Transport.GRPC,
+          options: {
+            url: configService.get<string>(
+              `TAVERN_${service}_GRPC_URL`,
+              `${service.toLowerCase()}-service:5000`,
+            ),
+            package: service.toLowerCase(),
+            protoPath: join(
+              workspaceRoot,
+              `proto/${service.toLowerCase()}.proto`,
+            ),
+          },
+        }),
+        inject: [ConfigService],
+      })),
+    ),
+  ],
   controllers: [],
   providers: [],
   exports: [],
