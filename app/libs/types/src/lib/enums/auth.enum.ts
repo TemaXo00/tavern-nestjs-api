89e7465719b200ae42a405f96e4dad0a38c72d33
temaxo <melnikov.artem294@gmail.com>
2026-08-29 11:18:13 +0300

    [feat] Add mapping enums for Roles

--- app/libs/types/src/lib/enums/auth.enum.ts
+++ app/libs/types/src/lib/enums/auth.enum.ts
@@ -1,5 +1,17 @@
 export enum Roles {
-  USER = 0,
-  ADMIN = 1,
-  MODERATOR = 2,
+  USER = 'USER',
+  ADMIN = 'ADMIN',
+  MODERATOR = 'MODERATOR',
 }
+
+export const ROLE_TO_GRPC: Record<Roles, number> = {
+  [Roles.USER]: 0,
+  [Roles.ADMIN]: 1,
+  [Roles.MODERATOR]: 2,
+};
+
+export const GRPC_TO_ROLE: Record<number, Roles> = {
+  0: Roles.USER,
+  1: Roles.ADMIN,
+  2: Roles.MODERATOR,
+};
