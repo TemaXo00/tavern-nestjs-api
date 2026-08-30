11079b5cc4897fa9206aab134de2949f9c552b89
temaxo <melnikov.artem294@gmail.com>
2026-08-30 12:17:51 +0300

    [feat] Created login and register methods

--- app/libs/auth-feature/src/lib/auth-feature.service.ts
+++ app/libs/auth-feature/src/lib/auth-feature.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from "@nestjs/common";
+import { type AuthOutput, type AuthServiceContract, type Empty, type ForgotPasswordInput, type LoginInput, type RefreshInput, type RegisterInput, type ResetPasswordInput, type UserEntity, type UserPayload, type ValidateInput } from "@org/types";
 
+import { AuthAuthorizeUtil } from "./utils/auth.util";
 import { AuthDatabaseUtil } from "./utils/database.util";
 import { AuthPasswordUtil } from "./utils/password.util";
 import { AuthValidateUtil } from "./utils/validate.util";
-
-import type { AuthOutput, AuthServiceContract, Empty, ForgotPasswordInput, LoginInput, RefreshInput, RegisterInput, ResetPasswordInput, UserEntity, UserPayload, ValidateInput } from "@org/types";
 
 @Injectable()
 export class AuthFeatureService implements AuthServiceContract {
@@ -12,7 +12,8 @@ export class AuthFeatureService implements AuthServiceContract {
   constructor(
     private readonly dbUtil: AuthDatabaseUtil,
     private readonly validationUtil: AuthValidateUtil,
-    private readonly passwordUtil: AuthPasswordUtil
+    private readonly passwordUtil: AuthPasswordUtil,
+    private readonly authUtil: AuthAuthorizeUtil
   ) {}
 
   async Register(data: RegisterInput): Promise<AuthOutput> {
@@ -20,15 +21,15 @@ export class AuthFeatureService implements AuthServiceContract {
     this.validationUtil.validatePasswordInput(data.password, data.passwordConfirmation)
     const hashedPassword = await this.passwordUtil.hashPassword(data.password)
     const newUser = await this.dbUtil.registerUser(data.email, hashedPassword)
-    return {
-      accessToken: newUser.id,
-      refreshToken: newUser.email
-    }
+    return await this.authUtil.authorizeNew(newUser, data.session)
   }
 
-  Login(data: LoginInput): Promise<AuthOutput> {
-    throw new Error("Method not implemented.",);
+  async Login(data: LoginInput): Promise<AuthOutput> {
+    const existingUser = await this.validationUtil.validateEmailFound(data.email)
+    await this.validationUtil.validatePassword(existingUser.passwordHash, data.password)
+    return await this.authUtil.authorizeNew(existingUser, data.session)
   }
+
   Refresh(data: RefreshInput): Promise<AuthOutput> {
     throw new Error("Method not implemented.");
   }
