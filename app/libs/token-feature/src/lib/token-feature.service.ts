4dfc480de227181e2425e85c869e654d414a527d
temaxo <melnikov.artem294@gmail.com>
2026-09-20 10:31:50 +0300

    [refactor] Using map util in token feature

--- app/libs/token-feature/src/lib/token-feature.service.ts
+++ app/libs/token-feature/src/lib/token-feature.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { AuthValidateService } from "@org/auth-core";
-import { Token } from "@org/auth-database";
-import { AuthDatabaseUtil, AuthMessagesUtil, AuthValidateUtil } from "@org/auth-utils";
+import { AuthDatabaseUtil, AuthMapUtil, AuthMessagesUtil, AuthValidateUtil } from "@org/auth-utils";
 import { AllTokensOutput, DeleteAllNotActiveTokensInput, DeleteTokenInput, Empty, GetTokensInput, RevokeTokenInput, Roles, TOKEN_STATE_TO_GRPC, TokenByIdInput, TokenOutput, TokenServiceContract, TokenStates } from '@org/types'
 
 @Injectable()
@@ -10,7 +9,8 @@ export class TokenFeatureService implements TokenServiceContract {
     private readonly validation: AuthValidateService,
     private readonly dbUtil: AuthDatabaseUtil,
     private readonly validateUtil: AuthValidateUtil,
-    private readonly messagesUtil: AuthMessagesUtil
+    private readonly messagesUtil: AuthMessagesUtil,
+    private readonly mapUtil: AuthMapUtil
   ) {}
 
   async GetTokensWithPagination(data: GetTokensInput): Promise<AllTokensOutput> {
@@ -23,7 +23,7 @@ export class TokenFeatureService implements TokenServiceContract {
     const payload = await this.validation.validateWithRoles(data.validation, [Roles.ADMIN])
     const token = await this.validateUtil.validateTokenFound(data.id, 'id')
     this.messagesUtil.sendAdminGetToken({adminId: payload.id, tokenId: data.id})
-    return this.mapToken(token)
+    return this.mapUtil.mapToken(token)
   }
 
   async SetTokenRevoked(data: RevokeTokenInput): Promise<TokenOutput> {
@@ -31,7 +31,7 @@ export class TokenFeatureService implements TokenServiceContract {
     await this.validateUtil.validateTokenFound(data.id, 'id')
     const token = await this.dbUtil.updateTokenState(data.id, TokenStates.REVOKED)
     this.messagesUtil.sendAdminRevokeToken({adminId: payload.id, tokenId: data.id})
-    return this.mapToken(token)
+    return this.mapUtil.mapToken(token)
   }
 
   async DeleteTokenById(data: DeleteTokenInput): Promise<TokenOutput> {
@@ -39,7 +39,7 @@ export class TokenFeatureService implements TokenServiceContract {
     await this.validateUtil.validateTokenFound(data.id, 'id')
     const token = await this.dbUtil.removeToken(data.id)
     this.messagesUtil.sendAdminDeleteToken({adminId: payload.id, tokenId: data.id})
-    return this.mapToken(token)
+    return this.mapUtil.mapToken(token)
   }
 
   async DeleteAllNotActiveTokens(data: DeleteAllNotActiveTokensInput): Promise<Empty> {
@@ -47,12 +47,5 @@ export class TokenFeatureService implements TokenServiceContract {
     const deleteAmount = await this.dbUtil.removeInactiveTokens()
     this.messagesUtil.sendAdminDeleteInactiveTokens({adminId: payload.id, tokensAmount: deleteAmount})
     return {}
-  }
-
-  private mapToken(token: Token): TokenOutput {
-    return {
-      ...token,
-      state: TOKEN_STATE_TO_GRPC[token.state]
-    }
   }
 }
