56f4fa2e766a88948665f4dac53c353ac69e0654
temaxo <melnikov.artem294@gmail.com>
2026-09-24 08:43:32 +0300

    [feat] Add output types for gateway

--- app/libs/types/src/lib/auth/authorization/auth-gateway.data.ts
+++ app/libs/types/src/lib/auth/authorization/auth-gateway.data.ts
@@ -1,6 +1,15 @@
 import { ForgotPasswordInput, LoginInput, RegisterInput, ResetPasswordInput } from "./auth-input.data.js";
+import { AuthOutput } from "./auth-output.data.js";
+import { UserEntity } from "./auth.entity.js";
+import { Roles } from "../../enums/auth.enum.js";
+import { Replace } from "../../utils/replace.js";
 
+// Input
 export type AuthRegisterGateway = Omit<RegisterInput, 'session'>
 export type AuthLoginGateway = Omit<LoginInput, 'session'>
 export type AuthForgotPasswordGateway = ForgotPasswordInput
 export type AuthResetPasswordGateway = Omit<ResetPasswordInput, 'token' | 'session'>
+
+// Output
+export type AuthGatewayOutput = Omit<AuthOutput, 'refreshToken'>
+export type UserEntityGateway = Replace<UserEntity, {role: Roles}>
