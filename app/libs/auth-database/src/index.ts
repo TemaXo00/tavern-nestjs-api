9b604b1e2e4b7e2ffad8729cdd2e1fab7385a91e
temaxo <melnikov.artem294@gmail.com>
2026-09-25 21:03:36 +0300

    [fix] Fixed export types from database

--- app/libs/auth-database/src/index.ts
+++ app/libs/auth-database/src/index.ts
@@ -1,10 +1,5 @@
 export * from './lib/auth-database.module';
 export * from './lib/auth-database.service';
-export { OAuthProviders, TokenState, Roles } from './generated/prisma/client';
-export type {
-  User,
-  OAuthAccount,
-  Session,
-  Token,
-} from './generated/prisma/client';
+export { TokenState, Roles } from './generated/prisma/client';
+export type { User, Session, Token } from './generated/prisma/client';
 export * from './generated/prisma/models';
