f90fbf0f11308cfc7b0fff9ef5e7624ef6fe5005
temaxo <melnikov.artem294@gmail.com>
2026-09-25 20:47:38 +0300

    [fix] Add import for local name dto

--- app/libs/dto/src/index.ts
+++ app/libs/dto/src/index.ts
@@ -3,3 +3,5 @@ export * from './lib/auth/authorization/forgot-password.dto.js';
 export * from './lib/auth/authorization/login.dto.js';
 export * from './lib/auth/authorization/register.dto.js';
 export * from './lib/auth/authorization/reset-password.dto.js';
+
+export * from './lib/auth/session/local-name.dto.js';
