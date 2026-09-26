4b9ac06e7f037992000bda0fafef17f43c9444c9
temaxo <melnikov.artem294@gmail.com>
2026-09-26 19:50:10 +0300

    [feat] Add imports for user dto

--- app/libs/dto/src/index.ts
+++ app/libs/dto/src/index.ts
@@ -7,3 +7,8 @@ export * from './lib/auth/authorization/reset-password.dto.js';
 export * from './lib/auth/session/local-name.dto.js';
 
 export * from './lib/auth/token/token-pagination.dto.js';
+
+export * from './lib/auth/user/block-user.dto.js';
+export * from './lib/auth/user/change-email.dto.js';
+export * from './lib/auth/user/change-password.dto.js';
+export * from './lib/auth/user/user-pagination.dto.js';
